208f4ff703211bfe35ebbf8bf97efeb2e796c3c4
comels <come.l.s@hotmail.fr>
2022-06-13 07:41:07 +0000

    betty bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,38 @@
 #include "sort.h"
-
 /**
- * bubble_sort - main function that sort
- * @array: array to sort
- * @size: size of the array
- * Return: void
+ * swap - function that swap two int
+ * @i: the fisrt num
+ * @j: the second num
  */
-
-void swap(int *xp, int *yp)
+void swap(int *i, int *j)
 {
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
+	int tmp;
 
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+/**
+ * bubble_sort - function that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: the array to sort
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned long int i, j;
+	size_t i, j;
 
-	if (size > 2)
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			// Last i elements are already in place
-			for (j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap(&array[j], &array[j + 1]);
-					print_array(array, size);
-				}
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 			}
 		}
 	}
